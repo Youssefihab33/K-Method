df78e086198dee99b89dc60ab80828ee30ea517c
Youssef Ihab <youssefihab33@gmail.com>
2026-09-14 11:29:37 +0300

    Altered the admin link in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -29,10 +29,11 @@ export default function Navbar() {
                         </Button>
                         {isAuthenticated ? (
                             <>
-                                {/* <Button color="primary" component={RouterLink} to="/dashboard">
-                                    Dashboard
-                                </Button> */}
-
+                                {isStaff && 
+                                    <Button color="primary" component={RouterLink} to="/admin">
+                                        Admin
+                                    </Button>
+                                }
                                 <IconButton onClick={handleMenu} sx={{ p: 0 }}>
                                     <Avatar sx={{ bgcolor: 'primary.main', width: 36, height: 36 }}>
                                         {user?.first_name?.[0] || user?.email?.[0]?.toUpperCase()}
@@ -40,7 +41,6 @@ export default function Navbar() {
                                 </IconButton>
                                 <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleClose}>
                                     <MenuItem onClick={() => { handleClose(); navigate('/profile'); }}>Profile</MenuItem>
-                                    {isStaff && <MenuItem onClick={() => { handleClose(); navigate('/auth/admin'); }}>Admin</MenuItem>}
                                     <MenuItem onClick={handleLogout}>Logout</MenuItem>
                                 </Menu>
                             </>
